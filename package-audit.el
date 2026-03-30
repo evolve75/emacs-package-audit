5aa93e1b283d212dc8d80dba213c1b3b36ff8690
Anupam Sengupta <anupamsg@gmail.com>
2026-03-30 14:41:06 -0400

    package-audit: add GNU Emacs disclaimer to all files
    Add the standard ";; This file is not part of GNU Emacs." line
    to all package-audit .el files, following the convention used
    by third-party packages.
    
    The line is placed after the file metadata (Author, Keywords,
    SPDX-License-Identifier) and before the license boilerplate.

--- package-audit.el
+++ package-audit.el
@@ -8,6 +8,8 @@
 ;; Keywords: convenience, tools, maint
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
+;; This file is not part of GNU Emacs.
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
